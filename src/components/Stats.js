8e730bc6bc39e3f7a233697ff8fa0e0efcfe2a9b
adnan-khattak <adnanktk540@gmail.com>
2023-08-26 16:33:25 +0500

    Statement Added

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -1,7 +1,17 @@
-export default function Stats() {
+import React from "react";
+export default function Stats({ totalEntered, selectedCount }) {
+  const percentage = (selectedCount / totalEntered) * 100;
   return (
     <footer className="stats">
-      <em>packing statements</em>
+      {/* <em>packing statements</em> */}
+      {selectedCount === totalEntered ? (
+        <p>You are ready to go! ✈️</p>
+      ) : (
+        <p>
+          You have selected {selectedCount} out of {totalEntered} items.
+          You are {percentage.toFixed(2)}% ready to go.
+        </p>
+      )}
     </footer>
   );
 }
